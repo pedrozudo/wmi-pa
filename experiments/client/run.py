c33950b547b66b3df397a1ee5e9f492e52606c05
Samuel Kolb <samuel.kolb@me.com>
2018-01-24 17:40:49 +0100

    Added problem definitions, parsing and client

---
+++ experiments/client/run.py
@@ -0,0 +1,70 @@
+from __future__ import print_function, division
+
+import json
+import argparse
+import pysmt.shortcuts as smt
+
+from sys import path
+
+import re
+
+path.insert(0, "../../src/")
+
+import problem
+import parse
+from wmi import WMI
+
+
+def substitute_special_names(nested_string):
+    replacement = "_symbol"
+    return re.sub("pi", "pi{}".format(replacement), nested_string)
+
+
+def convert(nested_string):
+    return parse.nested_to_smt(substitute_special_names(nested_string))
+
+
+def run(filename):
+    """
+
+    :param filename: The filename containing the problem, queries and weight function
+    :return:
+    """
+    with open(filename) as f:
+        flat = json.load(f)
+
+    domain = problem.import_domain(flat["domain"])
+
+    if "formula" in flat:
+        formula = convert(flat["formula"])
+    else:
+        formula = smt.TRUE()
+
+    support = []
+    for v in domain.real_vars:
+        lb, ub = domain.var_domains[v]
+        sym = smt.Symbol(substitute_special_names(v), domain.var_types[v])
+        support.append(lb <= sym)
+        support.append(sym <= ub)
+    formula = smt.simplify(smt.And(formula, *support))
+
+    if "weights" in flat:
+        weights = convert(flat["weights"])
+    else:
+        weights = smt.Real(1.0)
+
+    if "query" in flat:
+        query = convert(flat["query"])
+        wmi = WMI()
+        total_volume, _ = wmi.compute(formula, weights, WMI.MODE_PA)
+        query_volume, _ = wmi.compute(formula & query, weights, WMI.MODE_PA)
+        print(query_volume / total_volume)
+    else:
+        print("No queries or tasks detected.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    run(args.filename)
