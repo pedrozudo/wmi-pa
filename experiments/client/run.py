31b2f76637339e0a60d453d421746b8ac6e6e771
Samuel Kolb <samuel.kolb@me.com>
2018-01-25 17:46:36 +0100

    Encapsulated computation from parsing

--- experiments/client/run.py
+++ experiments/client/run.py
@@ -22,12 +22,28 @@ def substitute_special_names(nested_string):
 
 
 def convert(nested_string):
-    return parse.nested_to_smt(substitute_special_names(nested_string))
+    return parse.nested_to_smt(substitute_special_names(str(nested_string)))
+
+
+def compute_wmi(domain, queries, formula=None, weight_function=None):
+    support = []
+    for v in domain.real_vars:
+        lb, ub = domain.var_domains[v]
+        sym = smt.Symbol(substitute_special_names(v), domain.var_types[v])
+        support.append(lb <= sym)
+        support.append(sym <= ub)
+    formula = smt.simplify(smt.And(formula, *support))
+
+    wmi = WMI()
+    total_volume, _ = wmi.compute(formula, weight_function, WMI.MODE_PA)
+    for query in queries:
+        query_volume, _ = wmi.compute(formula & query, weight_function, WMI.MODE_PA)
+        yield query_volume / total_volume
 
 
 def run(flat):
     """
-
+    Computes the WMI using JSON input
     :param flat: A dict containing the string encoded domain, query / queries and optional formula and weight function
     :return:
     """
@@ -38,40 +54,39 @@ def run(flat):
     else:
         formula = smt.TRUE()
 
-    support = []
-    for v in domain.real_vars:
-        lb, ub = domain.var_domains[v]
-        sym = smt.Symbol(substitute_special_names(v), domain.var_types[v])
-        support.append(lb <= sym)
-        support.append(sym <= ub)
-    formula = smt.simplify(smt.And(formula, *support))
-
     if "weights" in flat:
         weights = convert(flat["weights"])
     else:
         weights = smt.Real(1.0)
 
     if "query" in flat:
-        query = convert(flat["query"])
-        wmi = WMI()
-        total_volume, _ = wmi.compute(formula, weights, WMI.MODE_PA)
-        query_volume, _ = wmi.compute(formula & query, weights, WMI.MODE_PA)
-        print(query_volume / total_volume)
+        queries = [convert(flat["query"])]
+    elif "queries" in flat:
+        queries = [convert(query_string) for query_string in flat["queries"]]
     else:
         print("No queries or tasks detected.")
+        return
+
+    i = 0
+    for p in compute_wmi(domain, queries, formula, weights):
+        i += 1
+        print("p(q{}): {}".format(i, p))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--filename", default=None)
-    parser.add_argument("-s", "--json_string", default=None)
+    def _parse():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-f", "--filename", default=None)
+        parser.add_argument("-s", "--json_string", default=None)
 
-    args = parser.parse_args()
-    if args.filename is not None:
-        with open(args.filename) as f:
-            flat = json.load(f)
-    elif args.json_string is not None:
-        flat = json.loads(args.json_string)
-    else:
-        raise RuntimeError("No input provided")
-    run(flat)
+        args = parser.parse_args()
+        if args.filename is not None:
+            with open(args.filename) as f:
+                flat = json.load(f)
+        elif args.json_string is not None:
+            flat = json.loads(args.json_string)
+        else:
+            raise RuntimeError("No input provided")
+        run(flat)
+
+    _parse()
